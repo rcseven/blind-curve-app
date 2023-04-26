bc17300b6acc235ffbaf9f70fa75d7563d87f19e
rcseven <rcedquila@gmail.com>
2023-04-26 16:30:49 +0800

    Initialize TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      background: "#F0F0F0",
+      primary: "#FFFFFF",
+      red: "#FF0000",
+      yellow: "#FFD700",
+      green: "#008000",
+    },
+    extend: {
+      dropShadow: {
+        primary: "0 0.5em 0.5em rgba(0, 0, 0, 0.25)",
+      },
+    },
+  },
+  plugins: [],
+};
